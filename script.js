f06a2714af43d65bb400a53d730d2f0342346613
Glitch (personal-site-example) <none>
2020-11-04 02:01:32 +0000

    🙍🍨 Checkpoint ./styles.css:1434827/148 ./script.js:1434827/144

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
 console.log('Mansi Website!');
 
 
-//on hover, highlights nav elements - HOME
-var container = document.getElementById("container");
-container.onmouseover = container.onmouseout = homeHover;
+// //on hover, highlights nav elements - HOME
+// var container = document.getElementById("container");
+// container.onmouseover = container.onmouseout = homeHover;
 
-function homeHover(H)
-{
- if (H.type =='mouseover'){
-   H.target.style.background = "#003462"
- }
-  if (H.type =='mouseout'){
-   H.target.style.background = '' 
+// function homeHover(H)
+// {
+//  if (H.type =='mouseover'){
+//    H.target.style.background = "#003462"
+//  }
+//   if (H.type =='mouseout'){
+//    H.target.style.background = '' 
     
-  }
-}
+//   }
+// }
 
-//on hover, highlights nav elements - ABOUT
-var container1 = document.getElementById("container1");
-container1.onmouseover = container1.onmouseout = homeHover1;
+// //on hover, highlights nav elements - ABOUT
+// var container1 = document.getElementById("container1");
+// container1.onmouseover = container1.onmouseout = homeHover1;
 
-function homeHover1(H)
-{
- if (H.type =='mouseover'){
-   H.target.style.background = "#C9C9F4"
- }
-  if (H.type =='mouseout'){
-   H.target.style.background = '' 
+// function homeHover1(H)
+// {
+//  if (H.type =='mouseover'){
+//    H.target.style.background = "#C9C9F4"
+//  }
+//   if (H.type =='mouseout'){
+//    H.target.style.background = '' 
     
-  }
-}
+//   }
+// }
 
-//on hover, highlights nav elements - ART
-var container2 = document.getElementById("container2");
-container2.onmouseover = container2.onmouseout = homeHover2;
+// //on hover, highlights nav elements - ART
+// var container2 = document.getElementById("container2");
+// container2.onmouseover = container2.onmouseout = homeHover2;
 
-function homeHover2(H)
-{
- if (H.type =='mouseover'){
-   H.target.style.background = "#C9C9F4"
- }
-  if (H.type =='mouseout'){
-   H.target.style.background = '' 
+// function homeHover2(H)
+// {
+//  if (H.type =='mouseover'){
+//    H.target.style.background = "#C9C9F4"
+//  }
+//   if (H.type =='mouseout'){
+//    H.target.style.background = '' 
     
-  }
-}
+//   }
+// }
 
-//on hover, highlights nav elements - COMPUTER SCIENCE
-var container3 = document.getElementById("container3");
-container3.onmouseover = container3.onmouseout = homeHover3;
+// //on hover, highlights nav elements - COMPUTER SCIENCE
+// var container3 = document.getElementById("container3");
+// container3.onmouseover = container3.onmouseout = homeHover3;
 
-function homeHover3(H)
-{
- if (H.type =='mouseover'){
-   H.target.style.background = "#C9C9F4"
- }
-  if (H.type =='mouseout'){
-   H.target.style.background = '' 
+// function homeHover3(H)
+// {
+//  if (H.type =='mouseover'){
+//    H.target.style.background = "#C9C9F4"
+//  }
+//   if (H.type =='mouseout'){
+//    H.target.style.background = '' 
     
-  }
-}
+//   }
+// }
